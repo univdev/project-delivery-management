4f8015160fc1f4c62402673b60a4f6df5999e9c6
박찬영 <chanyoung@starshell.co>
2019-06-01 00:29:48 +0900

    CompanySchema 제작

---
+++ src/com/delivery/java/db/schema/CompanySchema.java
@@ -0,0 +1,29 @@
+package com.delivery.java.db.schema;
+
+public class CompanySchema {
+
+	private int idx = 0;
+	private int uid = 0;
+	private String name = null;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
