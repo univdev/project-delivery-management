0593ec08d8f5f9b7b4feec45ab7dfc6abfa28d28
minhyuk Choi <m120971209@gmail.com>
2019-06-06 19:57:49 +0900

    CommentFrame 최대 입력 글자수 제한
    부실한 부분이 있지만 시간관계상 ㅎㅎ

--- src/com/delivery/java/layout/CommentFrame.java
+++ src/com/delivery/java/layout/CommentFrame.java
@@ -4,11 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -27,13 +30,36 @@ public class CommentFrame extends JFrame {
 		
 		JPanel labelPanel = new JPanel();
 		labelPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
-		JLabel label = new JLabel("할 말씀이 있다면 입력해주세요");
+		JLabel label = new JLabel("할 말씀이 있다면 입력해주세요. (최대 66자까지 입력 가능)");
 		label.setHorizontalAlignment(JLabel.CENTER);
 		labelPanel.add(label);
 		
 		JPanel commentPanel = new JPanel();
 		commentPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
 		commentField = new JTextField();
+		commentField.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				if(((JTextField)e.getSource()).getText().length() >= 66) {
+					JOptionPane.showMessageDialog(null, "최대 66자까지 입력 가능합니다.", "할 말 입력", JOptionPane.ERROR_MESSAGE);
+					e.consume();
+				}
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		commentPanel.add(commentField);
 		
 		JPanel buttonPanel = new JPanel();
